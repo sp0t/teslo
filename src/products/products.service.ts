3b488e748214cb12e3f6ccf1c74a043a33fb0f99
sp0t <kkostyukov13@gmail.com>
2024-10-04 19:47:29 -0500

    feat: update product endpoint

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,5 @@
 import {
+  BadRequestException,
   Injectable,
   InternalServerErrorException,
   Logger,
@@ -75,8 +76,22 @@ export class ProductsService {
     }
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async update(id: string, updateProductDto: UpdateProductDto) {
+    try {
+      const product = await this.productRepository.preload({
+        id: id,
+        ...updateProductDto,
+      });
+
+      if (!product) {
+        throw new NotFoundException(`Product with the id ${id} not found`);
+      }
+
+      await this.productRepository.save(product);
+      return product;
+    } catch (error) {
+      this.handleDBExceptions(error);
+    }
   }
 
   async remove(id: string) {
